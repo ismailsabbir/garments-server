ce1d8373ffeff5fdfeef832a3f503cd35480c13d
Your Name <you@example.com>
2023-09-01 19:59:34 +0600

    single service get api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.3w5podw.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,6 +29,12 @@ async function run() {
       const query = {};
       const services = await servicescollections.find(query).toArray();
       res.send(services);
+    });
+    app.get("/serviceDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await servicescollections.findOne(query);
+      res.send(service);
     });
     app.get("/projects", async (req, res) => {
       const query = {};
